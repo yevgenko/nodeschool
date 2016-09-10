878f70582fd661893fdccf007eec6b6ec9a86319
Yevhen V <wik@bdhr.co>
2016-09-10 01:04:57 +0100

    concat

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,26 +1,10 @@
-var through = require('through2'),
-    split = require('split'),
-    stream = through(write),
-    lineCounter = 0;
+var concat = require('concat-stream');
 
-function isEven(n) { return n % 2 == 0; }
-
-function changeLine(line) {
-  lineCounter += 1;
-
-  if (isEven(lineCounter)) {
-    return line.toUpperCase();
-  } else {
-    return line.toLowerCase();
-  }
-}
-
-function write(line, _, next) {
-  this.push(changeLine(line.toString()) + '\n');
-  next();
+function reverse(str) {
+  return str.split('').reverse().join('');
 }
 
 process.stdin
-  .pipe(split())
-  .pipe(stream)
-  .pipe(process.stdout)
+  .pipe(concat(function (src) {
+    console.log(reverse(src.toString()))
+  }));
