4e344c97671d60779420ea781f9d4c6446d936d6
Yevhen V <wik@bdhr.co>
2016-09-11 17:01:24 +0100

    crypt

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,36 +1,4 @@
-var combine = require('stream-combiner'),
-    split = require('split'),
-    through = require('through2').obj,
-    zlib = require('zlib');
+var crypto = require('crypto'),
+    stream = crypto.createDecipher('aes256', process.argv[2]);
 
-module.exports = function () {
-  var currentGenre;
-  var reduce = through(write, end);
-
-  function write(line, _, next) {
-    if (line.length === 0) return next();
-
-    var obj = JSON.parse(line);
-    if (obj.type === 'genre') {
-      if (currentGenre) {
-        this.push(JSON.stringify(currentGenre) + '\n');
-      }
-      currentGenre = { name: obj.name, books: [] };
-    } else if (obj.type === 'book') {
-      currentGenre.books.push(obj.name);
-    }
-
-    next();
-  }
-
-  function end(done) {
-    // the last genre object
-    if (currentGenre) {
-      this.push(JSON.stringify(currentGenre) + '\n');
-    }
-
-    done();
-  }
-
-  return combine(split(), reduce, zlib.createGzip());
-};
+process.stdin.pipe(stream).pipe(process.stdout);
